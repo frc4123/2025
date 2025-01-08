9ca786a3c17b55e40a54ff6738c35b3ee8b45581
AlphaLQ <sjb2019dpo@gmail.com>
2025-01-08 11:37:27 -0800

    drivetrain parameter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,41 +4,46 @@
 
 package frc.robot;
 
-import frc.robot.Commands.swerve.CommandSwerveDrivetrain;
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.Commands.autos.MiddleCoral;
+import frc.robot.Commands.swerve.SwerveCommand;
 import frc.robot.Subsystems.SwerveSubsystem;
-
-import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 
 
 public class RobotContainer {
-    private double MaxSpeed = SwerveSubsystem.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDeadband(SwerveConstants.kMaxSpeed * 0.1).withRotationalDeadband(SwerveConstants.kMaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-    private final Telemetry logger = new Telemetry(MaxSpeed);
+    private final Telemetry logger = new Telemetry(SwerveConstants.kMaxSpeed);
 
     private final CommandXboxController joystick = new CommandXboxController(Constants.InputConstants.kDriverControllerPort0);
 
-    public final CommandSwerveDrivetrain drivetrain = SwerveSubsystem.createDrivetrain();
+    private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem();
+    private final SwerveCommand drivetrain = SwerveSubsystem.createDrivetrain();
+    
+
+    private final SendableChooser<Command> m_autoChooser = new SendableChooser<Command>();
 
     public RobotContainer() {
         configureBindings();
+        initializeAutoChooser();
     }
 
     private void configureBindings() {
@@ -46,9 +51,9 @@ public class RobotContainer {
         // Y is left - WPILIB
         drivetrain.setDefaultCommand(
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(-joystick.getLeftY() * SwerveConstants.kMaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-joystick.getLeftX() * SwerveConstants.kMaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-joystick.getRightX() * SwerveConstants.kMaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
@@ -70,7 +75,18 @@ public class RobotContainer {
         drivetrain.registerTelemetry(logger::telemeterize);
     }
 
+    
+
+    public void initializeAutoChooser(){
+        m_autoChooser.setDefaultOption(
+      "middleCoral", new WaitCommand(0.1)
+      .andThen(new MiddleCoral(drivetrain).middleCoral())
+      );
+
+        SmartDashboard.putData("Auto Selector", m_autoChooser);
+    }
+
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        return m_autoChooser.getSelected();
     }
 }
